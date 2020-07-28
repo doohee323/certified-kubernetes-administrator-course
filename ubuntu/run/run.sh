9ea4da49d22af6631be811a436651797c567531d
Dewey Hong <doohee323@gmail.com>
2020-07-27 21:16:10 -0700

    .

--- ubuntu/run/run.sh
+++ ubuntu/run/run.sh
@@ -2,7 +2,6 @@
 set -e
 #set -x
 
-
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo " 01-client-tools.sh "
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
@@ -10,7 +9,7 @@ ssh -i ../../.vagrant/machines/kubemaster/virtualbox/private_key \
     vagrant@192.168.56.2 /bin/bash /vagrant/ubuntu/run/01-client-tools.sh
     
 echo "appending master's .ssh/id_rsa.pub to other nodes' authorized_keys!"
-for e in kubemaster kubenode01 loadbalancer; do
+for e in kubemaster kubenode01 kubenode02 loadbalancer; do
     if [[ ${e} == 'kubemaster' ]]; then
         ip=192.168.56.2
         instance='kubemaster'
@@ -27,7 +26,6 @@ done
 
 rm -Rf authorized_keys
 
-
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo " 02-creating_single_control_plane_cluster.sh "
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
@@ -36,10 +34,10 @@ ssh -i ../../.vagrant/machines/kubemaster/virtualbox/private_key vagrant@192.168
 sleep 3
 ssh -i ../../.vagrant/machines/kubenode01/virtualbox/private_key vagrant@192.168.56.3 \
         /bin/bash /vagrant/ubuntu/run/02.2-creating_single_control_plane_cluster.sh
-        
+ssh -i ../../.vagrant/machines/kubenode02/virtualbox/private_key vagrant@192.168.56.4 \
+        /bin/bash /vagrant/ubuntu/run/02.2-creating_single_control_plane_cluster.sh
 echo sleep 30
 sleep 30
-
 ssh -i ../../.vagrant/machines/kubemaster/virtualbox/private_key vagrant@192.168.56.2 \
         /bin/bash /vagrant/ubuntu/run/02.3-creating_single_control_plane_cluster.sh
 
@@ -50,5 +48,4 @@ ssh -i ../../.vagrant/machines/kubemaster/virtualbox/private_key vagrant@192.168
         /bin/bash /vagrant/ubuntu/run/3-smoke-test.sh
 
 
-exit 0
 
